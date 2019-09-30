8d8c78fd751835302470e832974ac523eb33bb21
pphaal <peter.phaal@inmon.com>
2019-09-30 16:03:15 -0700

    Add flow export

--- scripts/export.js
+++ scripts/export.js
@@ -1,4 +1,15 @@
+// author: InMon Corp.
+// version: 0.8
+// date: 10/30/2019
+// description: Prometheus exporter
+// copyright: Copyright (c) 2019 InMon Corp. ALL RIGHTS RESERVED
+
 function prometheusMetric(val) {
+
+  // ignore flow metrics
+  if(val.topKeys) return null;
+
+  // ignore string metrics
   if('number' !== typeof val.metricValue) return null;
 
   var result =  val.metricName.replace(/[^a-zA-Z0-9:_]/g,'_');
@@ -54,6 +65,96 @@ function getDump(agents,names,filter) {
   return result.join('\n');
 }
 
+var specID = 0;
+var userFlows = {};
+var SEP = '_SEP_';
+
+function flowSpecName(keys,value,filter,n,t) {
+
+  if(!keys || !value) return null;
+
+  var key = ''+keys+'#'+value+'#'+filter+'#'+n+'#'+t;
+  var entry = userFlows[key];
+  if(!entry) {
+    // try to create flow
+    var name = 'prometheus_' + specID;
+    try {
+      setFlow(name,{keys:keys, value:value, filter: filter, t:t, n:n, fs:SEP});
+      entry = {name:name};
+      userFlows[key] = entry;
+      specID++;
+    } catch(e) {
+      logInfo(e);
+      entry = null;
+    }
+  }
+  if(!entry) return null;
+  entry.lastQuery = Date.now();
+
+  return entry.name;
+}
+
+function deleteUserFlows(now) {
+  var key, entry;
+  for(key in userFlows) {
+    entry = userFlows[key];
+    if(now - entry.lastQuery > 300000) {
+      clearFlow(entry.name);
+      delete userFlows[key];
+    }
+  }
+}
+
+setIntervalHandler(function(now) {
+  deleteUserFlows(now);
+});
+
+function prometheusFlow(metric,keynames,flow,scale) {
+  var i, result = metric.replace(/[^a-zA-Z0-9:_]/g,'_'), keys = flow.key.split(SEP);
+  result += '{';
+  for(i = 0; i < keys.length; i++) {
+    if(i > 0) result += ',';
+    result += keynames[i].replace(/[^a-zA-Z0-9:_]/g,'_') + '="'+keys[i]+'"';
+  }
+  result += '} ' + (flow.value * scale);
+  return result;
+}
+
+function getFlows(agents,query) {
+  if(!query.metric) throw 'bad_request';
+
+  var metric = query.metric[0];
+  var labels = query.label ? query.label.join(',') : null;
+  var keys = query.key ? query.key.join(',') : null;
+  var value = query.value ? query.value[0] : null;
+  var filter = query.filter ? query.filter.join('&') : null;
+  var n = query.n ? query.n[0] : 10;
+  var t = query.t ? query.t[0] : 15;
+  var filter = query.filter ? query.filter[0] : null;
+  var aggMode = query.aggMode ? query.aggMode[0] : 'max';
+  var maxFlows = query.maxFlows ? query.maxFlows[0] : 20;
+  var minValue = query.minValue ? query.minValue[0] : 0.1;
+  var scale = query.scale ? parseFloat(query.scale[0]) : 1.0;
+  
+  var spec_name = flowSpecName(keys,value,filter,n,t);
+  if(!spec_name) throw 'bad_request';
+
+  var i, keynames = keys.split(',');
+  if(labels) {
+    labels = labels.split(',');
+    for(i = 0; i < keynames.length; i++) {
+      if(labels[i] && labels[i].length) keynames[i] = labels[i];
+    }
+  }
+
+  var result = [];
+  var flows = activeFlows(agents,spec_name,maxFlows,minValue,aggMode);
+  for(i = 0; i < flows.length; i++) {
+    result.push(prometheusFlow(metric,keynames,flows[i],scale));
+  }
+  return result.join('\n'); 
+}
+
 setHttpHandler(function(req) {
   var result, path = req.path;
   if(!path || path.length === 0) throw 'not_found';
@@ -61,6 +162,10 @@ setHttpHandler(function(req) {
     case 'dump':
       if(path.length !== 3) throw 'not_found';
       result = getDump(path[1],path[2],req.query);
+      break;
+    case 'flows':
+      if(path.length !== 2) throw 'not_found';
+      result = getFlows(path[1],req.query);
       break;
     default: throw 'not_found';
   }
