4fbca0a26e8d0bc92a20baa1ee5a9ee5bd83f208
pphaal <peter.phaal@inmon.com>
2019-10-19 08:09:20 -0700

    Integrate with native prometheus export

--- scripts/export.js
+++ scripts/export.js
@@ -1,48 +1,13 @@
 // author: InMon Corp.
-// version: 1.0
-// date: 10/8/2019
+// version: 1.1
+// date: 10/19/2019
 // description: Prometheus exporter
 // copyright: Copyright (c) 2019 InMon Corp. ALL RIGHTS RESERVED
 
-const prefix = getSystemProperty('prometheus.prefix') || 'sflow_';
+const prefix = getSystemProperty('prometheus.metric.prefix') || 'sflow_';
 
 function fixName(str) {
   return str.replace(/[^a-zA-Z0-9_]/g,'_');
-}
-
-function getAnalyzer() {
-  var res = analyzer();
-
-  var result = prefix+'rt_uptime_ms '+res.uptime+'\n';
-  result += prefix+'rt_agents '+res.sFlowAgents+'\n';
-  result += prefix+'rt_datagrams_received '+res.sFlowDatagramsReceived+'\n';
-  result += prefix+'rt_bytes_received '+res.sFlowBytesReceived+'\n';
-  result += prefix+'rt_parse_errors '+res.sFlowParseErrors+'\n';
-  result += prefix+'rt_unsupported_version '+res.sFlowUnsupportedVersion+'\n';
-  result += prefix+'rt_datagrams_discarded '+res.sFlowDatagramsDiscarded+'\n';
-  result += prefix+'rt_http_connections_current '+res.httpConnectionsCurrent+'\n';
-  result += prefix+'rt_http_connections_total '+res.httpConnectionsTotal+'\n';
-  result += prefix+'rt_http_messages_sent '+res.httpMessagesSent+'\n';
-  result += prefix+'rt_http_messages_received '+res.httpMessagesReceived+'\n';
-  result += prefix+'rt_http_bytes_sent '+res.httpBytesSent+'\n';
-  result += prefix+'rt_http_bytes_received '+res.httpBytesReceived+'\n';
-  result += prefix+'rt_available_processors '+res.availableProcessors+'\n';
-  result += prefix+'rt_heap_max '+res.heapMax+'\n';
-  result += prefix+'rt_heap_used '+res.heapUsed+'\n';
-  result += prefix+'rt_thread_total_started '+res.threadTotalStartedCount+'\n';
-  result += prefix+'rt_thread_count '+res.threadCount+'\n';
-  result += prefix+'rt_thread_daemon_count '+res.threadDaemonCount+'\n';
-  result += prefix+'rt_gc_time_ms '+res.gcTime+'\n';
-  result += prefix+'rt_gc_count '+res.gcCount+'\n';
-  result += prefix+'rt_flows_generated '+res.flowsGenerated+'\n';
-  result += prefix+'rt_events_generated '+res.eventsGenerated+'\n';
-  if(res.hasOwnProperty('cpuTime')) result += prefix+'rt_cpu_time_ms '+res.cpuTime+'\n';
-  if(res.hasOwnProperty('memTotal')) result += prefix+'rt_mem_total '+res.memTotal+'\n';
-  if(res.hasOwnProperty('memFree')) result += prefix+'rt_mem_free '+res.memFree+'\n';
-  if(res.hasOwnProperty('cpuLoadSystem')) result += prefix+'rt_cpu_load_system '+res.cpuLoadSystem+'\n';
-  if(res.hasOwnProperty('cpuLoadProcess')) result += prefix+'rt_cpu_load_process '+res.cpuLoadProcess+'\n';
-
-  return result;
 }
 
 function getMetric(group,agents,names,filter) {
@@ -55,67 +20,6 @@ function getMetric(group,agents,names,filter) {
 
     let rec = prefix+fixName(val.metricName)+'{group="'+group+'"} '+val.metricValue+'\n';
     result.push(rec); 
-  }
-  return result.join('');
-}
-
-function prometheusMetric(val) {
-
-  // ignore flow metrics
-  if(val.topKeys) return null;
-
-  // ignore string metrics
-  if('number' !== typeof val.metricValue) return null;
-
-  var result =  prefix+fixName(val.metricName);
-  result += '{agent="'+val.agent+'",datasource="'+val.dataSource+'"';
-  var host = metric(val.agent,'2.1.host_name')[0].metricValue;
-  if(!host) {
-    let nnames = topologyNodesForAgent(val.agent);
-    host = nnames && nnames.length === 1 ? nnames[0] : null; 
-  }
-  if(host != null) result += ',host="'+host+'"';
-  if(val.dataSource.indexOf('.') === -1) {
-    let ifname = ifName(val.agent,val.dataSource);
-    if(ifname) result += ',ifname="'+ifname+'"';
-  } else if(val.dataSource.startsWith('3.')) {
-    let vir_host = metric(val.agent,val.dataSource+'.vir_host_name')[0].metricValue;
-    if(vir_host) {
-      if(vir_host.startsWith('k8s_')) {
-        // Parse Kubernetes dockershim name
-        // https://github.com/kubernetes/kubernetes/blob/master/pkg/kubelet/dockershim/naming.go
-        if(vir_host.startsWith('k8s_POD_')) {
-          // Sandbox
-          // k8s_POD_{s.name}_{s.namespace}_{s.uid}_{s.attempt}
-          return null;
-        } else {
-          // Container
-          // k8s_{c.name}_{s.name}_{s.namespace}_{s.uid}_{c.attempt}
-          let [,name,,namespace] = vir_host.split('_');
-          if(name) result += ',k8s_name="'+name+'"';
-          if(namespace) result += ',k8s_namespace="'+namespace+'"';
-        }
-      } else {
-        // Check for Docker Swarm mode name
-        let swarm = vir_host.match(/^([^\.]+)\.([^\.]{25})\.([^\.]{25})$/);
-        if(swarm) {
-          result += ',swarm_name="'+swarm[1]+'"'; 
-        } else {
-          result += ',vir_host="'+vir_host+'"';
-        }
-      }
-    }
-  }
-  result += '} ' + val.metricValue + '\n';
-  return result;
-}
-
-function getDump(agents,names,filter) {
-  var i, val, result = [], vals = dump(agents,names,filter);
-  for(i = 0; i < vals.length; i++) {
-    val = prometheusMetric(vals[i]);
-    if(!val) continue;
-    result.push(val);
   }
   return result.join('');
 }
@@ -214,17 +118,9 @@ setHttpHandler(function(req) {
   var result, path = req.path;
   if(!path || path.length === 0) throw 'not_found';
   switch(path[0]) {
-    case 'analyzer':
-      if(path.length !== 1) throw 'now_found';
-      result = getAnalyzer();
-      break;
     case 'statistics':
       if(path.length !== 4) throw 'not_found'
       result = getMetric(path[1],path[2],path[3],req.query);
-      break;
-    case 'dump':
-      if(path.length !== 3) throw 'not_found';
-      result = getDump(path[1],path[2],req.query);
       break;
     case 'flows':
       if(path.length !== 2) throw 'not_found';
